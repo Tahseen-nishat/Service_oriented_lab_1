213f6ecc61df06f3a1ffac9cc90ae09113433c07
SHAIK TAHSEEN NISHAT <106258164+Tahseen-nishat@users.noreply.github.com>
2023-08-19 23:53:41 +0530

    Add files via upload
    Running the HTML Page:
    
    Open healthcare.html in a web browser.
    Enter patient information in the form.
    Click "Save Data and Display Table".
    Patient data will be saved as patient_data.xml and displayed below.
    Running the Java Code:
    
    Compile ParseXML.java using the command: javac ParseXML.java
    Run the compiled class using the command: java ParseXML
    Open patient_table.html to view the patient data table.

---
+++ week1/ParseXML.java
@@ -0,0 +1,65 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ParseXML {
+    public static void main(String[] args) {
+        try {
+            File xmlFile = new File("patient_data.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(xmlFile);
+
+            NodeList nodeList = document.getElementsByTagName("patient");
+
+            StringBuilder htmlTable = new StringBuilder();
+            htmlTable.append("<table border='1'>");
+            htmlTable.append("<tr><th>Name</th><th>Age</th><th>Symptoms</th></tr>");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    String name = element.getElementsByTagName("name").item(0).getTextContent();
+                    String age = element.getElementsByTagName("age").item(0).getTextContent();
+                    String symptoms = element.getElementsByTagName("symptoms").item(0).getTextContent();
+
+                    htmlTable.append("<tr><td>").append(name).append("</td><td>").append(age).append("</td><td>")
+                            .append(symptoms).append("</td></tr>");
+                }
+            }
+
+            htmlTable.append("</table>");
+
+            writeHtmlTableToFile(htmlTable.toString(), "patient_table.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeHtmlTableToFile(String tableContent, String fileName) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<style>");
+            writer.println("</style>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println(tableContent);
+            writer.println("</body>");
+            writer.println("</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
